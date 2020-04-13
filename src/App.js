cabb3b74f21b8ebc75481afa024c85289a9810a7
Stanislav Mochyr <delenjer@gmail.com>
2020-04-13 20:07:12 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from 'react';
 import './App.css';
 
 const App = () => (
-  <h1>Count: 0</h1>
+  <>
+    <h1>Count: 0</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere maxime
+      molestiae mollitia omnis perferendis! Accusantium, aperiam dicta
+      dignissimos eveniet fugit,
+      impedit ipsam nesciunt odio perferendis, porro sint soluta velit vero.
+    </p>
+  </>
 );
 
 export default App;
